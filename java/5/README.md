8e1b06819931628073c03dbe344532eb965fd8ca
ecoder321 <femiwonderful@gmail.com>
2021-07-12 12:07:49 -0400

    test 5 is an amazon debugging practice

---
+++ java/5/README.md
@@ -0,0 +1,3 @@
+## Amazon Debugging Prep
+
+Write a code thst generates the HCF of numbers in an array
---
+++ java/5/src/App.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Array;
+
+public class App {
+    static int calculateHCF(int a, int b) {
+        if (a == 0) return b;
+        return calculateHCF(b % a, a);
+    }
+
+    static int calculateGeneralHCF(int len, int[] arr) {
+        int hcf = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            hcf = calculateHCF(hcf, arr[i]);
+        }
+        return hcf;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int len = calculateHCF(25, 15);
+        int[] arr = new int[] {14, 21, 28, 42, 63};
+        System.out.println(calculateGeneralHCF(len, arr));
+        // int[] arr2 = new int[] {};
+        // System.out.println(arr2[0]);
+    }
+}

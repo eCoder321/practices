4374febfe2bb393d1374cd6d91cef1195f0ee367
ecoder321 <femiwonderful@gmail.com>
2021-07-17 00:31:22 -0400

    created a js folder and completed a test

---
+++ javascript/1/solution.js
@@ -0,0 +1,16 @@
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function accepts following parameters:
+ *  1. DOUBLE meal_cost
+ *  2. INTEGER tip_percent
+ *  3. INTEGER tax_percent
+ */
+
+function solve(meal_cost, tip_percent, tax_percent) {
+    // Write your code here
+    const additions = (tax_percent + tip_percent) / 100
+    // console.log(additions)
+    const finalAmount = meal_cost + (meal_cost * additions)
+    console.log(Math.round(finalAmount))
+}
